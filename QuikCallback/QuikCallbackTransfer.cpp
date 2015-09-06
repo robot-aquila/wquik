cffccd15e9ac4e257f3dc541ddf3ad2fc29632e4
Dmitry Zolotarev <dmitry.zolotarev@gmail.com>
2015-09-06 13:11:34 +0300

    quik callback: added critical section

--- QuikCallback/QuikCallbackTransfer.cpp
+++ QuikCallback/QuikCallbackTransfer.cpp
@@ -1,17 +1,30 @@
 #include "QuikCallbackTransfer.h"
+#include "aquila/core/auto_drop.h"
+#include "aquila/core/auto_grab.h"
 #include "aquila/core/WinApi.h"
 #include "aquila/core/Lock.h"
 #include "aquila/core/CriticalSection.h"
 
+using namespace aquila::core;
+
 static wquik::IQuikCallbackHandler* handler = NULL;
+static auto_drop<IWinApi> p_winApi;
+static auto_grab<ICriticalSection> p_cs;
 
 bool APIENTRY DllMain(HANDLE hModule, DWORD dwReason, LPVOID lpReserved) {
-   return TRUE;
+	switch (dwReason) {
+	case DLL_PROCESS_ATTACH:
+		p_winApi = WinApi::getInstance();
+		p_cs = new CriticalSection(p_winApi);
+		break;
+	}
+	return TRUE;
 }
 
 QUIKCALLBACKTRANSFER_API void __stdcall ConnectionStatusCallback
 	(long nConnectionEvent, long nExtendedErrorCode, LPSTR lpstrInfoMessage)
 {
+	Lock x(p_cs);
 	if ( handler ) {
 		handler->onConnectionStatus
 			(nConnectionEvent, nExtendedErrorCode, lpstrInfoMessage);
@@ -23,6 +36,7 @@ QUIKCALLBACKTRANSFER_API void __stdcall TransactionReplyCallback
 	long nTransactionReplyCode, DWORD dwTrandId, double dOrderNum,
 	LPSTR lpstrTransactionReplyMessage)
 {
+	Lock x(p_cs);
 	if ( handler ) {
 		handler->onTransactionReply
 			(nTransactionResult, nTransactionExtendedErrorCode,
@@ -36,6 +50,7 @@ QUIKCALLBACKTRANSFER_API void __stdcall OrderStatusCallback
 	LPSTR lpstrSecCode, double dPrice, long nBalance, double dValue,
 	long nIsSell, long nStatus, long nOrderDescriptor)
 {
+	Lock x(p_cs);
 	if ( handler ) {
 		handler->onOrderStatus(nMode, dwTrandId, dNumber, lpstrClassCode,
 			lpstrSecCode, dPrice, nBalance, dValue, nIsSell, nStatus,
@@ -48,6 +63,7 @@ QUIKCALLBACKTRANSFER_API void __stdcall TradeStatusCallback
 	LPSTR lpstrSecCode, double dPrice, long nQty, double dValue, long nIsSell,
 	long nTradeDescriptor)
 {
+	Lock x(p_cs);
 	if ( handler ) {
 		handler->onTradeStatus(nMode, dNumber, dOrderNum, lpstrClassCode,
 			lpstrSecCode, dPrice, nQty, dValue, nIsSell, nTradeDescriptor);
@@ -57,6 +73,7 @@ QUIKCALLBACKTRANSFER_API void __stdcall TradeStatusCallback
 QUIKCALLBACKTRANSFER_API void __stdcall setHandler
 	(wquik::IQuikCallbackHandler* ch)
 {
+	Lock x(p_cs);
 	handler = ch;
 }
 
